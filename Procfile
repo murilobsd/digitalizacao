f18ec77799d28eea27ef81f6e0536beef8766cca
murilobsd <kernelobsd@gmail.com>
2013-11-25 10:17:53 -0200

    Iniciando o repositório

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn digitalizacao.wsgi
---
+++ digitalizacao/settings.py
@@ -0,0 +1,55 @@
+# encoding: utf-8
+from unipath import Path
+
+BASE_DIR = Path(__file__).parent
+
+SECRET_KEY = '!$+^r&h*q*&n$z+29q0z(n@@b!+n$qj297nk(&=9%wsf=7^7b_'
+
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'digitalizacao.core',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'digitalizacao.urls'
+
+WSGI_APPLICATION = 'digitalizacao.wsgi.application'
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR.child('db.sqlite3'),
+    }
+}
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+STATIC_URL = '/static/'
